519840e0ce15028c03f6ad29b5b87c298fdeacb4
dimamas996 <dimamas996@yandex.ru>
2022-10-19 23:36:27 +0300

    layout edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import "./App.css";
+import Color from "color";
+
+/* const styles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}; */
 
 function App() {
+  function randInRange(start = 0, end = 255) {
+    return Math.floor(Math.random() * end + start);
+  }
+
+  function arrayOfRandRGB() {
+    return [randInRange(), randInRange(), randInRange()];
+  }
+
+  let color = Color(arrayOfRandRGB());
+  console.log(color);
+
+/*   let colorStr1 = {
+    backgroundColor: color.rgb(),
+  };
+
+  let colorStr2 = {
+    backgroundColor: color.negate(),
+  }; */
+
   return (
-    <div>
-      <h1>HW!</h1>
-    </div>
+    <>
+      <header className="hg__header"></header>
+      <main className="hg__main"></main>
+      <aside className="hg__left"></aside>
+      <aside className="hg__right"></aside>
+      <footer className="hg__footer"></footer>
+    </>
   );
 }
 
